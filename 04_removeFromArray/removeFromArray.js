4fef4a87928cf71725bc7cb45ade161f34fafeaf
FarazAkhtarKhan <fk782911@gmail.com>
2025-02-15 19:09:29 +0530

    First commit, including Excersise 1
    Majority text case passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, number) {
+        return arr.filter(item => item !== number)
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(lowerlimit, upperlimit) {
+    let sum =0;
+    if(lowerlimit && upperlimit !== Number) return 'ERROR';
+    else if(lowerlimit > upperlimit) {
+        temp =  lowerlimit; 
+        lowerlimit = upperlimit;
+        upperlimit = temp;
+    }
+    else if((upperlimit || lowerlimit) < 0) return 'ERROR'
+    for(let i=lowerlimit; i <= upperlimit; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
